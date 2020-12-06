b62ed1d02305344b69cbf50b6dfb40d5b481df14
Anil Lama <heaven.gunsa@gmail.com>
2020-12-06 20:36:29 +0200

    changed User table to userTable name to make compatible with heroku postgres

--- src/main/java/fi/haagahelia/moviedatabase/domain/User.java
+++ src/main/java/fi/haagahelia/moviedatabase/domain/User.java
@@ -9,8 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name ="usertable")
 public class User {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
