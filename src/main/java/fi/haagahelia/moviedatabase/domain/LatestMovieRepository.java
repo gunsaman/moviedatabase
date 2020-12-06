6646980aa545c8dd452641870763e6d6f932724f
gunsaman <heaven.gunsa@gmail.com>
2020-12-06 22:09:21 +0200

    added latest movie feature

---
+++ src/main/java/fi/haagahelia/moviedatabase/domain/LatestMovieRepository.java
@@ -0,0 +1,7 @@
+package fi.haagahelia.moviedatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LatestMovieRepository extends CrudRepository<LatestMovie, Long> {
+
+}
